2930a305166405a0a4c144c2ab0a70d657c6bcc4
stanislav <stanislav.davydenko@nerdpeople.com>
2024-07-24 21:07:59 +0100

    separated module, controller and service for movies created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MoviesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
