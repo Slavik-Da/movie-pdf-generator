8f748c1d31557232d196c57bd26c34587067a18d
stanislav <stanislav.davydenko@nerdpeople.com>
2024-07-24 21:20:31 +0100

    movies module separated

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Res,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Response } from 'express';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  async getPopularMovies(@Res() res: Response): Promise<void> {
+    try {
+      const pdf = await this.moviesService.getPopularMoviesPdf();
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader(
+        'Content-Disposition',
+        'attachment; filename=popular_movies.pdf',
+      );
+      res.end(pdf);
+    } catch (error) {
+      throw new HttpException(
+        'Error fetching movies',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  @Get('/:id')
+  async getMoviesById(
+    @Param('id') id: string,
+    @Res() res: Response,
+  ): Promise<void> {
+    try {
+      const pdf = await this.moviesService.getMovieByIdPdf(id);
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader(
+        'Content-Disposition',
+        `attachment; filename=movie_${id}.pdf`,
+      );
+      res.end(pdf);
+    } catch (error) {
+      throw new HttpException(
+        'Error fetching movies',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
